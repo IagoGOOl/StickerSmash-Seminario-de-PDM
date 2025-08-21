ec337918bc075ca0aaf053f16a5b7e50c2d76fcb
Isaelson Trajano <trajanocds@MacBook-Air-de-Isaelson.local>
2025-08-21 01:32:00 -0300

    Adicionando o modal

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,15 +1,20 @@
-import { StyleSheet, View } from 'react-native';
+import Button from '@/components/Button';
+import CircleButton from '@/components/CircleButton';
+import EmojiList from '@/components/EmojiList';
+import EmojiPicker from '@/components/EmojiPicker';
+import IconButton from '@/components/IconButton';
+import ImageViewer from '@/components/ImageViewer';
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
-
-import Button from '@/components/Button';
-import ImageViewer from '@/components/ImageViewer';
+import { ImageSourcePropType, StyleSheet, View } from 'react-native';
 
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
-  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
-
+   const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined)
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false)
+  const [isModelVisible, setIsModelVisible] = useState<boolean>(false)
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSourcePropType | undefined>(undefined);
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ["images"],
@@ -17,22 +22,52 @@ export default function Index() {
       quality: 1,
     });
 
-    if (!result.canceled) {
+      if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     } else {
       alert("You did not select any image.");
     }
+  };
+  
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onModalClose = async () => {
+    setIsModelVisible(false)
+  }
+
+  const onAddSticker = () => {
+    setIsModelVisible(true)
+  };
+
+  const onSaveImageAsync = async () => {
   };
 
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        
       </View>
+      {showAppOptions ? (
+      <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+        ) : (
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
+          <Button label='Choose a photo' theme='primary' onPress={pickImageAsync} />
+          <Button label='Use this photo' onPress={() => setShowAppOptions(true)} />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModelVisible} onClose={onModalClose}>
+          <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+      </EmojiPicker>
     </View>
   );
 }
@@ -50,4 +85,15 @@ const styles = StyleSheet.create({
     flex: 1 / 3,
     alignItems: 'center',
   },
+
+   optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+
 });
